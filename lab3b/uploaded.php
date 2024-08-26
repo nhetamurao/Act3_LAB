ab7b05595541e558a4262ae8ab7f548d03d3be86
nhetamurao <nethamurao@gmail.com>
2024-08-26 21:17:06 +0800

    Image File

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -1,6 +1,6 @@
 <?php
 $upload_directory = getcwd() . '/uploads/';
-$relative_path = '/uploads/';
+$relative_path = 'uploads/';
 
 // Ensure the uploads directory exists
 if (!is_dir($upload_directory)) {
@@ -33,15 +33,28 @@ if (move_uploaded_file($temporary_audio_file, $uploaded_audio_file)) {
     echo 'Failed to upload audio file';
 }
 
-// Handle Image File
-$uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
-$temporary_image_file = $_FILES['image_file']['tmp_name'];
 
-if (move_uploaded_file($temporary_image_file, $uploaded_image_file)) {
-    echo "<img src='{$relative_path}" . basename($_FILES['image_file']['name']) . "' alt='image' />";
+
+
+$uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
+$temporary_file = $_FILES['image_file']['tmp_name'];
+
+if (move_uploaded_file($temporary_file, $uploaded_image_file)) {
+    echo '<h3>Uploaded Image File:</h3>';
+    echo '<img src="' . $relative_path . basename($_FILES['image_file']['name']) . '" width="600" height="400">';
 } else {
     echo 'Failed to upload image file';
 }
+
+// // Handle Image File
+// $uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
+// $temporary_image_file = $_FILES['image_file']['tmp_name'];
+
+// if (move_uploaded_file($temporary_image_file, $uploaded_image_file)) {
+//     echo "<img src='{$relative_path}" . basename($_FILES['image_file']['name']) . "' alt='image' />";
+// } else {
+//     echo 'Failed to upload image file';
+// }
 
 echo '<pre>';
 var_dump($_FILES);
